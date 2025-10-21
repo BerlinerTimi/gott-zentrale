3bf00fd5010a2f7393b0cd12644e2c6febc88c78
Timothy Brian Knopp <67046967+BerlinerTimi@users.noreply.github.com>
2025-10-21 21:59:45 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import express from "express";
 const app = express();
+
 app.get("/", (_req, res) => res.send("🪽 Gott-Zentrale online"));
-app.listen(3000, () => console.log("Server läuft auf Port 3000"));
+app.get("/health", (_req, res) => res.json({ status: "ok", time: Date.now() }));
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
